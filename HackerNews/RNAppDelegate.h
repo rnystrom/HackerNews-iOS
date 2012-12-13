857459ff6b0403ecae715d5958de067924f01555
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-12-13 16:41:35 -0500

    init

---
+++ HackerNews/RNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RNAppDelegate.h
+//  HackerNews
+//
+//  Created by Ryan Nystrom on 12/13/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackerNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HackerNews
+//
+//  Created by Ryan Nystrom on 12/13/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RNAppDelegate class]));
+    }
+}
